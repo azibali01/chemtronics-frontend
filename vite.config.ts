487cf6c8d598dbf6ca37bc02b62f6f8907173d7e
azibali01 <azibaliansari311@gmail.com>
2025-10-08 09:06:50 +0500

    Fix routing for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // 👈 important for correct routing on deployment
   optimizeDeps: {
     include: ["jspdf", "jspdf-autotable", "html2canvas", "dompurify", "canvg"],
   },
