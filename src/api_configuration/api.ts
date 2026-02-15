29a2a9de3ad1482f498c7a9f1f4a3ef46c485bfa
wasif296 <wasifzahoor296@gmail.com>
2026-02-16 02:01:34 +0500

    test

--- src/api_configuration/api.ts
+++ src/api_configuration/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://chemtronics-backend-xufv.onrender.com",
   withCredentials: true, // ✅ Important for CORS + cookies
 });
 
